bf7cb8836234c2ae29b9467cc9d76747f4e2bca9
roznerx <ignaciorosner@gmail.com>
2021-07-27 16:56:23 -0300

    Started to code JS DOM, having trouble with the scope of the variables and constants

--- library.js
+++ library.js
@@ -1,15 +1,88 @@
-let myLibrary = ["The Hobbit", "The Black Emperor", "Legion"];
+let myLibrary = [];
 
-function Book() {
-  // the constructor...
+//Constructor
+
+function Book(title, author, genre, year, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.year = year;
+  this.pages = pages;
+  this.status = status;
 }
 
-function addBookToLibrary() {
-}
+const addBook = document.getElementById("addbook");
+const popup = document.querySelector(".popup-wrapper");
+const popupClose = document.querySelector(".popup-close");
 
-function displayBooks() {
-    myLibrary.forEach(book => console.table(book));
-}
+const newTitle = document.getElementById("newtitle");
+const newAuthor = document.getElementById("newauthor");
+const newGenre = document.getElementById("newgenre");
+const newYear = document.getElementById("newyear");
+const newPages = document.getElementById("newpages");
+const statusRead = document.getElementById("status1");
+const statusNotRead = document.getElementById("status2");
 
-displayBooks();
+const submit = document.getElementById("submit");
+
+
+//Popup
+
+addBook.addEventListener("click", () => {
+    popup.style.display = "block";
+});
+
+popupClose.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+popup.addEventListener("click", event => {
+  if(event.target.className === "popup-wrapper") {
+      popup.style.display = "none";
+  }
+});
+
+//Add book(s)
+
+submit.addEventListener("click", () => {
+
+  popup.style.display = "none";
+
+  let newTitleValue = newTitle.value;
+
+  let newAuthorValue = newAuthor.value;
+
+  let newGenreValue = newGenre.value;
+
+  //MAKE CONDITION FOR GENRE PICTURES
+
+  let newYearValue = newYear.value;
+
+  let newPagesValue = newPages.value;
+  
+  let addStatus = "";
+  if (statusRead.checked) {
+    addStatus = statusRead.value;
+  } else if (statusNotRead.checked) {
+    addStatus = statusNotRead.value;
+  }
+
+  let newBook = new Book(newTitleValue, newAuthorValue, newGenreValue, newYearValue, newPagesValue, addStatus);
+  myLibrary.push(newBook);
+
+  let ul = document.createElement("ul");
+  ul.innerHTML = newTitleValue;
+  ul.setAttribute("type","button");
+  ul.id = "listedBook";
+  document.getElementById("booklist").appendChild(ul); 
+});
+
+
+
+
+  const listedBook = document.getElementById("listedBook");
+  listedBook.addEventListener("click", () => {
+  console.log("TESTOK");
+});
+
 
